05082ba7cf9ef24ed65b11738d4e1e4a9e149238
Ryan Syme <33377621+NayMyers@users.noreply.github.com>
2017-11-28 12:20:23 +0000

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,13 @@
 using namespace std;
 
 #define MAZEDESCRIPTIONS "maze_descriptions.txt"
+#define MAZECONNECTIONS "maze_connections.txt"
+#define NODOOR -1
+#define NUMBEROFDOORS 5
+
+enum direction {NORTH,SOUTH,EAST,WEST,OUT};
+const vector<string> directions = { "NORTH", "SOUTH", "EAST", "WEST", "OUT" };
+const vector<string> shorthandDirections = { "N", "S", "E", "W", "O" };
 
 class Room
 {
@@ -35,9 +42,12 @@ class Maze
 {
 private:
 	vector<Room> Contents;
+	vector<Connection> Connections;
 public:
 	void displayAllRooms(void); //for testing purposes only
+	void displayAllConnections(void); //for testing purposes only
 	bool loadDescriptionsFromFile(void); //load in the room descriptions
+	bool loadConnectionsFromFile(void); //load room connections
 };
 
 bool Maze::loadDescriptionsFromFile(void)
@@ -87,6 +97,57 @@ private:
 	Maze CurrentMaze;
 public:
 };
+
+class Door
+{
+private:
+	int _leadsto = NODOOR; //an int value indicating room number door leads to 
+	bool _locked = false; //indicates whether door is locked 
+	string _keyrequired = ""; //name of key required to open door if locked 
+public:
+	Door(int leadsto, bool locked, string keyrequired);
+	int leadsto(void);
+	bool locked(void);
+	void unlockDoor(string key);
+};
+Door::Door(int leadsto, bool locked, string keyrequired)
+{
+	_leadsto = leadsto;
+	_locked = locked;
+	_keyrequired = keyrequired;
+}
+
+int Door::leadsto(void)
+{
+	return _leadsto;
+}
+bool Door::locked(void)
+{
+	return _locked;
+}
+void Door::unlockDoor(string key)
+{
+	if (!_locked)
+	{
+		cout << "Door is not locked!" << endl;
+		return;
+	}
+	if (key == _keyrequired)
+	{
+		cout << "You have the right key! The door unlocks" << endl;
+		_locked = false;
+		return;
+	}
+
+	cout << "you do not have the right key! The door remains locked!" << endl;
+}
+class Connection
+{
+public:
+	vector<Door> Direction;
+	//least knowledge does not apply because connection only being used to organise 2d vector 
+};
+
 int main(void)
 {
 	Maze MyMaze;
